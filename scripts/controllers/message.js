bb75f3124b18770ff13a6c1a3c08c6293af55495
Whitney <whitneywooyy@gmail.com>
2015-05-01 14:37:46 -0600

    Completed Part 2 of project - refactor

--- scripts/controllers/message.js
+++ scripts/controllers/message.js
@@ -2,8 +2,9 @@
 
 angular.module('chattyApp')
   .controller('MessageCtrl', function ($scope, MessageService) {
-    $scope.messages = [];
+    $scope.messages = ["why"];
     MessageService.getMessages().then(function(res){
     	$scope.messages = res.data;
+    	console.log("messages", $scope.messages);
     })
   });
--- server.js
+++ server.js
@@ -1,40 +1,82 @@
-var http = require('http');
-var messages = ["Suppp", "How you doing?", "I'm a punk!"];
+// EXPRESS.JS VERSION
 
-var onRequest = function(req, res){
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
-	if (req.method === "GET") {
-		res.writeHead(200, {
-			'Connection': 'close',
-			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*'
-		})
-	res.end(JSON.stringify({message: messages}));
-	}
-	else if (req.method === "POST") {
-		res.writeHead(200, {
-			'Connection': 'close',
-			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*'
-		})
-		var postData = '';
-		req.on('data', function(chunk) {
-			postData += chunk.toString();
-		});
-		req.on('end', function() {
-			console.log("Got POST data:");
-			console.log(JSON.parse(postData));
-			messages.push(JSON.parse(postData).message);
-			console.log(messages);
-			res.end(JSON.stringify({message: messages}));
-		});
+var messages = [];
+
+app.use(bodyParser());
+
+app.use(function(req, res, next){
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+})
+
+app.get('/', function(req, res){
+	res.send(JSON.stringify(messages));
+});	// End app.get
+
+app.post('/', function(req, res){
+	req.body.createdAt = new Date();
+	console.log(req.body);
+	messages.push(req.body);
+	res.send(JSON.stringify(messages));
+})
+
+app.listen(8080);
+console.log("Listening in on port " + 8080);
+
+
+// REGULAR NODE.JS VERSION
+
+// var http = require('http');
+// var messages = ["Suppp", "How you doing?", "I'm a punk!"];
+
+// var onRequest = function(req, res){
+
+// 	if (req.method === "GET") {
+// 		res.writeHead(200, {
+// 			'Connection': 'close',
+// 			'Content-Type': 'application/json',
+// 			'Access-Control-Allow-Origin': '*'
+// 		})
+// 	res.end(JSON.stringify({message: messages}));
+// 	}
+// 	else if (req.method === "POST") {
+// 		res.writeHead(200, {
+// 			'Connection': 'close',
+// 			'Content-Type': 'application/json',
+// 			'Access-Control-Allow-Origin': '*'
+// 		})
+// 		var postData = '';
+// 		req.on('data', function(chunk) {
+// 			postData += chunk.toString();
+// 		});
+// 		req.on('end', function() {
+// 			console.log("Got POST data:");
+// 			console.log(JSON.parse(postData));
+// 			messages.push(JSON.parse(postData).message);
+// 			console.log(messages);
+// 			res.end(JSON.stringify({message: messages}));
+// 		});
+// 		else if (req.method === "OPTIONS") {	// For Chrome browser
+// 		res.writeHead(200, {
+// 			'Connection': 'close',
+// 			'Content-Type': 'application/json',
+// 			'Access-Control-Allow-Origin': '*',
+// 			'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+// 			'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+// 		});	// End res.writeHead
 	
-	}
+// 	}
 
-};	// End onRequest
+// };	// End onRequest
 
 
 
-http.createServer(onRequest).listen(9200);
-console.log("Listening in on port " + 9200);
+// http.createServer(onRequest).listen(9200);
+// console.log("Listening in on port " + 9200);
 
